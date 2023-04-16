7c641af28942a99207d77ea795daf0f21794ee73
HFScripts <130812709+HFScripts@users.noreply.github.com>
2023-04-16 17:58:00 +1200

    Update services.py

--- services.py
+++ services.py
@@ -6,20 +6,22 @@ def run_services(site_ports, subdomains):
     # Loop through each domain and port combination
     for domain, ports in site_ports.items():
         for port in ports:
-            url = f"http://{domain}:{port}"
-            command = ['whatweb', url]
-            result = subprocess.run(command, capture_output=True, text=True)
-            for line in result.stdout.splitlines():
-                if domain in line and "The plain HTTP request was sent to HTTPS port" not in line and line not in unique_lines:
-                    print(line)
-                    unique_lines.add(line)
+            for protocol in ['http', 'https']:
+                url = f"{protocol}://{domain}:{port}"
+                command = ['whatweb', url]
+                result = subprocess.run(command, capture_output=True, text=True)
+                for line in result.stdout.splitlines():
+                    if domain in line and "The plain HTTP request was sent to HTTPS port" not in line and line not in unique_lines:
+                        print(line)
+                        unique_lines.add(line)
 
     # Loop through each subdomain
     for subdomain in subdomains:
-        url = f"http://{subdomain}"
-        command = ['whatweb', url]
-        result = subprocess.run(command, capture_output=True, text=True)
-        for line in result.stdout.splitlines():
-            if subdomain in line and "The plain HTTP request was sent to HTTPS port" not in line and line not in unique_lines:
-                print(line)
-                unique_lines.add(line)
+        for protocol in ['http', 'https']:
+            url = f"{protocol}://{subdomain}"
+            command = ['whatweb', url]
+            result = subprocess.run(command, capture_output=True, text=True)
+            for line in result.stdout.splitlines():
+                if subdomain in line and "The plain HTTP request was sent to HTTPS port" not in line and line not in unique_lines:
+                    print(line)
+                    unique_lines.add(line)
